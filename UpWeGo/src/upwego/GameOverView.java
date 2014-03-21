9d65dbf414b8dd9bbd4374e7b5dee27f295e330b
stefantouhey <stefan2e@earthlink.net>
2014-03-21 09:17:48 -0700

    gameover image

--- UpWeGo/src/upwego/GameOverView.java
+++ UpWeGo/src/upwego/GameOverView.java
@@ -1,7 +1,15 @@
 package upwego;
-import jgame.GContainer;
 
+import jgame.GContainer;
+import jgame.GSprite;
+import jgame.ImageCache;
 
 public class GameOverView extends GContainer {
+	public GameOverView() {
+		super(new GSprite(ImageCache.forClass(UpWeGo.class).get(
+				"GameOver.png")));
+		setSize(640, 640);
+
+	}
 
 }
