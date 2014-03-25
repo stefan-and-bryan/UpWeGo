ecadfc67e5a2acc41c95597437d35cf59d7a3d67
thechosenone124 <thechosenone124@gmail.com>
2014-03-25 10:32:36 -0700

    Just pull it

---
+++ UpWeGo/src/upwego/Floor.java
@@ -0,0 +1,15 @@
+package upwego;
+
+import jgame.GPaintable;
+
+/**
+ * An interface that indicates that the object is solid ground, and that objects
+ * controlled by a {@link PlatformController} should be able to stand on it and
+ * jump from it.
+ * 
+ * @author William Chargin
+ * 
+ */
+public interface Floor extends GPaintable {
+
+}
--- UpWeGo/src/upwego/Platform.java
+++ UpWeGo/src/upwego/Platform.java
@@ -4,7 +4,7 @@ import upwego.UpWeGo;
 import jgame.GSprite;
 import jgame.ImageCache;
 
-public class Platform extends GSprite {
+public class Platform extends GSprite implements Floor {
 	public Platform() {
 	super(new GSprite(ImageCache.forClass(UpWeGo.class).get(
 			"greensquare.jpg")));
