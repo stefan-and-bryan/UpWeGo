a5abfe6887ebd77a0eb0b11d0d4711463e71a326
thechosenone124 <thechosenone124@gmail.com>
2014-04-24 10:31:53 -0700

    Trying to make the character teleport to top of platform when he lands

--- UpWeGo/src/upwego/Protagonist.java
+++ UpWeGo/src/upwego/Protagonist.java
@@ -1,14 +1,28 @@
 package upwego;
 
+import jgame.Context;
+import jgame.GObject;
 import jgame.GSprite;
 import jgame.ImageCache;
+import jgame.listener.HitTestListener;
 
 public class Protagonist extends GSprite {
 	public Protagonist() {
 		super(new GSprite(ImageCache.forClass(UpWeGo.class).get(
 				"MAINCHARACTER.png")));
-		this.setSize(70,116);
+		setAnchorPosition(1,116);
+		this.setSize(1,116);
+		
+		HitTestListener OnTop = new HitTestListener(Platform.class) {
 
+			@Override
+			public void invoke(GObject target, Context context) {
+				// TODO Auto-generated method stub
+				target.setLocation(target.getX(),424);
+			}
+			
+		};
+		//addListener(OnTop);
 		
 	}
 	
